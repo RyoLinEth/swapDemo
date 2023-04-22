3f9584907fa8228d483a2c9368a32d3d3e9bce04
tangwanli <771147315@qq.com>
2023-04-22 12:16:48 +0800

    修复create-pools

--- apps/web/src/pages/createPools/StepFour.tsx
+++ apps/web/src/pages/createPools/StepFour.tsx
@@ -1,73 +1,71 @@
-import React from "react";
-import { Message, MessageText, Button } from '@pancakeswap/uikit'
+import React from 'react'
+import { Message, MessageText, Button, Row } from '@pancakeswap/uikit'
 
 const StepFour = (props) => {
-   const { isLoading, isRejected, setGoSteps, setIsRejected } = props
-   return (
-      <>
-        <style jsx global>
-          {`
-            .step-four-item {
-                display: flex;
-                flex-direction: column;
-                height: 100%
-            }
-            .step-four-item .content {
-               display: flex;
-               justify-content: center;
-               margin: 0 10px;
-            }
-            .step-four-item .footer {
-                display: flex;
-                justify-content: flex-end;
-                margin-top: auto;
-            }
-            .text-label {
-               margin-bottom: 5px
-            }
-          `}
-        </style>
-      <section className='step-four-item'>
-        <div className="row content">
-         {
-            isLoading && (
-            <Message variant="warning">
-               <MessageText>
-               loading
-               </MessageText>
+  const { isLoading, isRejected, setGoSteps, setIsRejected } = props
+  return (
+    <>
+      <style jsx global>
+        {`
+          .step-four-item {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+          }
+          .step-four-item .content {
+            display: flex;
+            justify-content: center;
+            margin: 0 10px;
+          }
+          .step-four-item .footer {
+            display: flex;
+            justify-content: flex-end;
+            margin-top: auto;
+          }
+          .text-label {
+            margin-bottom: 5px;
+          }
+        `}
+      </style>
+      <section className="step-four-item">
+        <Row className="row content">
+          {isLoading && (
+            // @ts-ignore
+            <Message variant="warning" style={{ width: '98%' }}>
+              <MessageText>loading</MessageText>
             </Message>
-         )
-         }
-         {
-            isRejected && (
-               <Message variant="danger">
-                  <MessageText>
-                  User Denied Transaction
-                  </MessageText>
-               </Message>
-            )
-         }
-         {
-            !isLoading && !isRejected && (
-               <Message variant="success">
-                  <MessageText>
-                  The pool is created successfully.
-                  </MessageText>
-               </Message>
-            )
-         }
-         </div>
+          )}
+          {isRejected && (
+            // @ts-ignore
+            <Message variant="danger" style={{ width: '98%' }}>
+              <MessageText>User Denied Transaction</MessageText>
+            </Message>
+          )}
+          {!isLoading && !isRejected && (
+            // @ts-ignore
+            <Message variant="success" style={{ width: '98%' }}>
+              <MessageText>The pool is created successfully.</MessageText>
+            </Message>
+          )}
+        </Row>
 
-         <div className="footer">
-            <Button mr="8px" type="button" variant="light" onClick={() => {
-                  setGoSteps(2)
-                  setIsRejected(false)
-               }}>Prev</Button>
-           {/* <Button type="button" variant="success" onClick={() => setGoSteps(4)}>Submit</Button> */}
-         </div>
+        <div className="footer">
+          <Button
+            mr="8px"
+            type="button"
+            variant="light"
+            onClick={() => {
+              setGoSteps(2)
+              setIsRejected(false)
+            }}
+          >
+            Prev
+          </Button>
+          {/* <Button type="button" variant="success" onClick={() => setGoSteps(4)}>Submit</Button> */}
+        </div>
       </section>
-      </>
-   );
-};
+    </>
+  )
+}
 
-export default StepFour;
+export default StepFour
--- apps/web/src/pages/createPools/StepTwo.tsx
+++ apps/web/src/pages/createPools/StepTwo.tsx
@@ -1,89 +1,102 @@
-import React, { useState } from "react";
-import { Button, Text, Input } from '@pancakeswap/uikit'
+import React, { useState } from 'react'
+import { Button, Text, Input, Row } from '@pancakeswap/uikit'
 
-const StepTwo = ({
-   setGoSteps,
-   defaultAccount,
-   ownerValue,
-   onSubmit
-}) => {
+const StepTwo = ({ setGoSteps, defaultAccount, ownerValue, onSubmit }) => {
+  const [owner, setOwner] = useState(ownerValue)
 
-   const [owner, setOwner] = useState(ownerValue)
+  const datas = [
+    {
+      title: 'Owner',
+      placeHolder: 'The owner of the staking contract',
+      type: 'text',
+      function: (e) => setOwner(e.target.value),
+      value: owner,
+    },
+  ]
 
-   const datas = [
-      {
-         title: "Owner",
-         placeHolder: "The owner of the staking contract",
-         type: 'text',
-         function: (e) => setOwner(e.target.value),
-         value: owner,
-      },
-   ]
-
-   return (
-      <>
-        <style jsx global>
-          {`
-            .step-two-item {
-                display: flex;
-                flex-direction: column;
-                height: 100%
-            }
-            .step-two-item .content {
-               display: flex;
-               justify-content: center;
-            }
-            .step-two-item .footer {
-                display: flex;
-                justify-content: flex-end;
-                margin-top: auto;
-            }
-            .text-label {
-               margin-bottom: 5px
-            }
-          `}
-        </style>
-      <section className='step-two-item'>
-         <div className="row content">
-            {
-               datas.map((data, index) => {
-                  return (
-                     <div
-                        key={data.title}
-                        className="col-lg-6 mb-2"
-                     >
-                        <div className="form-group mb-3">
-                           <Text bold style={{marginBottom: '8px'}}>{data.title}</Text>
-                           <Input
-                              placeholder={data.placeHolder}
-                              defaultValue={data.value}
-                              autoComplete="off"
-                              onChange={data.function}
-                              />
-                           {
-                              index === 0 && (
-                                 <Button mt="20px" id="Wallet" type="button" variant="success" onClick={() => setOwner(defaultAccount)}>Use Linking Wallet</Button>
-                              )
-                           }
-                        </div>
-                     </div>
-                  )
-               })
-            }
-         </div>
-         <div className='footer'>
-           <Button mr="8px" type="button" variant="light"  onClick={() => {
-               setGoSteps(0)
-               onSubmit(owner)
-            }}>Prev</Button>
-           <Button type="button" variant="success" onClick={() => {
-               setGoSteps(2)
-               onSubmit(owner)
-            }}>Next</Button>
-         </div>
+  return (
+    <>
+      <style jsx global>
+        {`
+          .step-two-item {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+          }
+          .step-two-item .content {
+            display: flex;
+            justify-content: center;
+          }
+          .step-two-item .footer {
+            display: flex;
+            justify-content: flex-end;
+            margin-top: auto;
+          }
+          .text-label {
+            margin-bottom: 5px;
+          }
+          .content-box {
+            width: 50%;
+          }
+        `}
+      </style>
+      <section className="step-two-item">
+        <Row className="row content">
+          {datas.map((data, index) => {
+            return (
+              <div key={data.title} className="content-box">
+                <div className="form-group mb-3">
+                  <Text bold style={{ marginBottom: '8px' }}>
+                    {data.title}
+                  </Text>
+                  <Input
+                    placeholder={data.placeHolder}
+                    defaultValue={data.value}
+                    autoComplete="off"
+                    onChange={data.function}
+                  />
+                  {index === 0 && (
+                    <Button
+                      mt="20px"
+                      id="Wallet"
+                      type="button"
+                      variant="success"
+                      onClick={() => setOwner(defaultAccount)}
+                    >
+                      Use Linking Wallet
+                    </Button>
+                  )}
+                </div>
+              </div>
+            )
+          })}
+        </Row>
+        <div className="footer">
+          <Button
+            mr="8px"
+            type="button"
+            variant="light"
+            onClick={() => {
+              setGoSteps(0)
+              onSubmit(owner)
+            }}
+          >
+            Prev
+          </Button>
+          <Button
+            type="button"
+            variant="success"
+            onClick={() => {
+              setGoSteps(2)
+              onSubmit(owner)
+            }}
+          >
+            Next
+          </Button>
+        </div>
       </section>
-      </>
-   );
-};
+    </>
+  )
+}
 
-export default StepTwo;
+export default StepTwo
